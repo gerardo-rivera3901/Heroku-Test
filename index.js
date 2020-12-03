d2e007e7f9c50d898ad5186ea7f035dc6a55d3d6
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2020-12-03 10:36:27 -0800

    WIP

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 4000;
+const path = require('path');
 
 // console.log('Hey!');
 // console.log(__dirname);
@@ -15,11 +16,16 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/*', (_, res) => {
   res.json({ data: 'Hey pal!' })
 });
 
+app.use('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+});
+
 app.listen(port, () => {
   console.log(`Check out port ${port}!`);
-})
+});
